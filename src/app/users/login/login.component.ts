263d68b956e967f76ff8152d449370b698dfc131
Emilio Calejero <milioce@gmail.com>
2021-11-23 20:11:22 +0100

    Add properties to TS

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  logged = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onLogin() {
   }
 
 }
